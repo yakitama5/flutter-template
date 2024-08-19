566f83fa5da328d98749354c71771daa4197c27e
yakitama5 <rittitarou@yahoo.co.jp>
2024-08-19 18:36:37 +0900

    ♻️generete: melos bs

--- packages/features/app_update/lib/i18n/strings.g.dart
+++ packages/features/app_update/lib/i18n/strings.g.dart
@@ -6,7 +6,7 @@
 /// Locales: 2
 /// Strings: 2 (1 per locale)
 ///
-/// Built on 2024-08-08 at 07:42 UTC
+/// Built on 2024-08-19 at 08:38 UTC
 
 // coverage:ignore-file
 // ignore_for_file: type=lint
