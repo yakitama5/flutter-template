9c8fa1f8b1d5933f557b3b133756db67c69a28e2
yakitama5 <rittitarou@yahoo.co.jp>
2024-09-13 18:47:10 +0900

    ♻️refactor: EdgeInsetsの固定指定を修正

--- packages/features/setting/lib/src/presentation/components/src/themed_settings_list.dart
+++ packages/features/setting/lib/src/presentation/components/src/themed_settings_list.dart
@@ -15,7 +15,7 @@ class ThemedSettingsList extends HookConsumerWidget {
   final ScrollPhysics? physics;
   final DevicePlatform? platform;
   final Brightness? brightness;
-  final EdgeInsets? contentPadding;
+  final EdgeInsetsGeometry? contentPadding;
   final List<AbstractSettingsSection> sections;
 
   @override
