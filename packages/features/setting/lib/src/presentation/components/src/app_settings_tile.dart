e0f5a90ff7f605b1f1d4896399fa6db75b266b84
yakitama5 <rittitarou@yahoo.co.jp>
2024-09-16 14:33:27 +0900

    🐛fix: `flutter_settings_ui`のtrailing表示バグを修正

---
+++ packages/features/setting/lib/src/presentation/components/src/app_settings_tile.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+import 'package:settings_ui/src/tiles/platforms/android_settings_tile.dart';
+import 'package:settings_ui/src/tiles/platforms/ios_settings_tile.dart';
+import 'package:settings_ui/src/tiles/platforms/web_settings_tile.dart';
+
+enum AppSettingsType {
+  navigation,
+  switchTile,
+}
+
+/// Custmize `settings_ui`
+class AppSettingsTile extends AbstractSettingsTile {
+  AppSettingsTile({
+    this.leading,
+    this.trailing,
+    this.value,
+    required this.title,
+    this.description,
+    this.onPressed,
+    this.enabled = true,
+    super.key,
+  }) {
+    onToggle = null;
+    initialValue = null;
+    activeSwitchColor = null;
+    tileType = SettingsTileType.simpleTile;
+  }
+
+  AppSettingsTile.navigation({
+    this.leading,
+    this.trailing,
+    this.value,
+    required this.title,
+    this.description,
+    this.onPressed,
+    this.enabled = true,
+    super.key,
+  }) {
+    type = AppSettingsType.navigation;
+  }
+
+  AppSettingsTile.switchTile({
+    required this.initialValue,
+    required this.onToggle,
+    this.activeSwitchColor,
+    this.leading,
+    this.trailing,
+    required this.title,
+    this.description,
+    this.onPressed,
+    this.enabled = true,
+    super.key,
+  }) {
+    type = AppSettingsType.switchTile;
+  }
+
+  late final AppSettingsType type;
+
+  final Widget? leading;
+  final Widget? trailing;
+  final Widget title;
+  final Widget? description;
+  final void Function(BuildContext context)? onPressed;
+
+  late final Color? activeSwitchColor;
+  late final Widget? value;
+  // ignore: avoid_positional_boolean_parameters
+  late final void Function(bool value)? onToggle;
+  late final SettingsTileType tileType;
+  late final bool? initialValue;
+  late final bool enabled;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = SettingsTheme.of(context);
+
+    switch (theme.platform) {
+      case DevicePlatform.android:
+      case DevicePlatform.fuchsia:
+      case DevicePlatform.linux:
+        return AndroidSettingsTile(
+          description: description,
+          onPressed: onPressed,
+          onToggle: onToggle,
+          tileType: tileType,
+          value: value,
+          leading: leading,
+          title: title,
+          enabled: enabled,
+          activeSwitchColor: activeSwitchColor,
+          initialValue: initialValue ?? false,
+          trailing: trailing,
+        );
+      case DevicePlatform.iOS:
+      case DevicePlatform.macOS:
+      case DevicePlatform.windows:
+        return IOSSettingsTile(
+          description: description,
+          onPressed: onPressed,
+          onToggle: onToggle,
+          tileType: tileType,
+          value: value,
+          leading: leading,
+          title: title,
+          trailing: trailing,
+          enabled: enabled,
+          activeSwitchColor: activeSwitchColor,
+          initialValue: initialValue ?? false,
+        );
+      case DevicePlatform.web:
+        return WebSettingsTile(
+          description: description,
+          onPressed: onPressed,
+          onToggle: onToggle,
+          tileType: tileType,
+          value: value,
+          leading: leading,
+          title: title,
+          enabled: enabled,
+          trailing: trailing,
+          activeSwitchColor: activeSwitchColor,
+          initialValue: initialValue ?? false,
+        );
+      case DevicePlatform.device:
+        throw Exception(
+          'You can\'t use the DevicePlatform.device in this context. '
+          'Incorrect platform: SettingsTile.build',
+        );
+    }
+  }
+}
