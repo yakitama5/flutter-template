2ce317c7ca2985698aaa68e89c48eeaa70c07a9b
yakitama5 <rittitarou@yahoo.co.jp>
2024-10-25 17:14:02 +0900

    🎨design: device_previewの追加

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -1,17 +1,12 @@
 import 'package:cores_core/application.dart';
-import 'package:cores_core/presentation.dart';
-import 'package:cores_designsystem/application.dart';
 import 'package:cores_designsystem/i18n.dart';
 import 'package:cores_designsystem/init.dart';
-import 'package:cores_designsystem/presentation.dart';
-import 'package:cores_error/application.dart';
 import 'package:cores_error/i18n.dart';
 import 'package:cores_firebase/init.dart';
 import 'package:cores_shared_preferences/init.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:features_app_update/i18n.dart';
 import 'package:features_app_update/init.dart';
-import 'package:features_app_update/presentation.dart';
 import 'package:features_goods/i18n.dart';
 import 'package:features_goods/init.dart';
 import 'package:features_maintenance/i18n.dart';
@@ -25,11 +20,10 @@ import 'package:flutter_app/app_initializer.dart';
 import 'package:flutter_app/i18n/strings.g.dart';
 import 'package:flutter_app/src/application/state/initial_location_provider.dart';
 import 'package:flutter_app/src/presentation/router/state/router_provider.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nested/nested.dart';
 
-import 'src/presentation/components/device_preview_wrapper.dart';
+import 'src/presentation/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -75,64 +69,6 @@ void main() async {
       ),
     ),
   );
-}
-
-class MainApp extends ConsumerWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // テーマ設定
-    final themeMode = ref.watch(themeModeProvider);
-    final uiStyle = ref.watch(uiStyleProvider);
-    final lightTheme =
-        ref.watch(appThemeProvider(brightness: Brightness.light));
-    final darkTheme = ref.watch(appThemeProvider(brightness: Brightness.dark));
-
-    ref
-        // エラー検知
-        // TODO(yakitama5): アプリ全体の例外管理に応じて変更検討(mixinかriverpod)
-        .listen<AppException?>(
-      appExceptionNotifierProvider,
-      (_, appException) {
-        if (appException != null) {
-          SnackBarManager.showErrorSnackBar(
-            'An error occurred: ${appException.message}',
-          );
-          ref.read(appExceptionNotifierProvider.notifier).consume();
-        }
-      },
-    );
-
-    return MaterialApp.router(
-      builder: (_, child) => Nested(
-        children: const [
-          // レスポンシブデザイン
-          ResponsiveAutoScaleBox(),
-          // 共通のローディング表示
-          LoaderOverlay(),
-          // アプリアップデートチェック
-          AppUpdateListner(),
-
-          // デバッグ用
-          DevicePreviewSingleChildContainer(),
-        ],
-        child: child,
-      ),
-
-      // Slang
-      locale: DevicePreview.locale(context) ??
-          TranslationProvider.of(context).flutterLocale,
-      supportedLocales: AppLocaleUtils.supportedLocales,
-      localizationsDelegates: GlobalMaterialLocalizations.delegates,
-
-      scaffoldMessengerKey: SnackBarManager.rootScaffoldMessengerKey,
-      routerConfig: ref.watch(routerProvider),
-      theme: lightTheme.copyWith(platform: uiStyle.platform),
-      darkTheme: darkTheme.copyWith(platform: uiStyle.platform),
-      themeMode: themeMode,
-    );
-  }
 }
 
 class _AppTranslationProvider extends SingleChildStatelessWidget {
---
+++ apps/app/lib/src/presentation/app.dart
@@ -0,0 +1,72 @@
+import 'package:cores_core/presentation.dart';
+import 'package:cores_designsystem/application.dart';
+import 'package:cores_designsystem/presentation.dart';
+import 'package:cores_error/application.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:features_app_update/presentation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/i18n/strings.g.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nested/nested.dart';
+
+import 'components/device_preview_wrapper.dart';
+import 'router/state/router_provider.dart';
+
+class MainApp extends ConsumerWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // テーマ設定
+    final themeMode = ref.watch(themeModeProvider);
+    final uiStyle = ref.watch(uiStyleProvider);
+    final lightTheme =
+        ref.watch(appThemeProvider(brightness: Brightness.light));
+    final darkTheme = ref.watch(appThemeProvider(brightness: Brightness.dark));
+
+    ref
+        // エラー検知
+        // TODO(yakitama5): アプリ全体の例外管理に応じて変更検討(mixinかriverpod)
+        .listen<AppException?>(
+      appExceptionNotifierProvider,
+      (_, appException) {
+        if (appException != null) {
+          SnackBarManager.showErrorSnackBar(
+            'An error occurred: ${appException.message}',
+          );
+          ref.read(appExceptionNotifierProvider.notifier).consume();
+        }
+      },
+    );
+
+    return MaterialApp.router(
+      builder: (_, child) => Nested(
+        children: const [
+          // レスポンシブデザイン
+          ResponsiveAutoScaleBox(),
+          // 共通のローディング表示
+          LoaderOverlay(),
+          // アプリアップデートチェック
+          AppUpdateListner(),
+
+          // デバッグ用
+          DevicePreviewSingleChildContainer(),
+        ],
+        child: child,
+      ),
+
+      // Slang
+      locale: DevicePreview.locale(context) ??
+          TranslationProvider.of(context).flutterLocale,
+      supportedLocales: AppLocaleUtils.supportedLocales,
+      localizationsDelegates: GlobalMaterialLocalizations.delegates,
+
+      scaffoldMessengerKey: SnackBarManager.rootScaffoldMessengerKey,
+      routerConfig: ref.watch(routerProvider),
+      theme: lightTheme.copyWith(platform: uiStyle.platform),
+      darkTheme: darkTheme.copyWith(platform: uiStyle.platform),
+      themeMode: themeMode,
+    );
+  }
+}
