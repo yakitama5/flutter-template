fc30bbbe2020a476c5bbe53453e3959e0c044643
yakitama5 <rittitarou@yahoo.co.jp>
2024-10-25 15:45:58 +0900

    🎨design: レスポンシブデザイン対応を追加

--- packages/cores/designsystem/lib/src/presentaion/components/src/responsive_auto_scale_box.dart
+++ packages/cores/designsystem/lib/src/presentaion/components/src/responsive_auto_scale_box.dart
@@ -19,41 +19,38 @@ class ResponsiveAutoScaleBox extends SingleChildStatelessWidget {
         const Breakpoint(start: 1921, end: double.maxFinite, name: UHD),
       ],
       child: Builder(
-        builder: (context) => MaxWidthBox(
-          maxWidth: 1920,
-          child: ResponsiveScaledBox(
-            width: ResponsiveValue<double>(
-              context,
-              defaultValue: 450,
-              conditionalValues: [
-                const Condition.equals(
-                  name: MOBILE,
-                  value: 450,
-                ),
-                const Condition.between(
-                  start: 700,
-                  end: 900,
-                  value: 700,
-                ),
-                const Condition.between(
-                  start: 901,
-                  end: 1200,
-                  value: 900,
-                ),
-                const Condition.between(
-                  start: 1201,
-                  end: 1400,
-                  value: 1300,
-                ),
-                const Condition.between(
-                  start: 1401,
-                  end: 1920,
-                  value: 1600,
-                ),
-              ],
-            ).value,
-            child: child ?? const SizedBox.shrink(),
-          ),
+        builder: (context) => ResponsiveScaledBox(
+          width: ResponsiveValue<double>(
+            context,
+            defaultValue: 450,
+            conditionalValues: [
+              const Condition.equals(
+                name: MOBILE,
+                value: 450,
+              ),
+              const Condition.between(
+                start: 700,
+                end: 900,
+                value: 700,
+              ),
+              const Condition.between(
+                start: 901,
+                end: 1200,
+                value: 900,
+              ),
+              const Condition.between(
+                start: 1201,
+                end: 1400,
+                value: 1300,
+              ),
+              const Condition.between(
+                start: 1401,
+                end: 1920,
+                value: 1600,
+              ),
+            ],
+          ).value,
+          child: child ?? const SizedBox.shrink(),
         ),
       ),
     );
