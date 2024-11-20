bef559e0c24d499487fab67018a857837210571d
yakitama5 <rittitarou@yahoo.co.jp>
2024-11-20 18:39:43 +0900

    🗨️comment: TODOコメントの追加

--- packages/cores/designsystem/lib/src/infrastructure/shared_preferences/repository/shared_preferences_theme_repository.dart
+++ packages/cores/designsystem/lib/src/infrastructure/shared_preferences/repository/shared_preferences_theme_repository.dart
@@ -12,6 +12,7 @@ class SharedPreferencesThemeRepository implements ThemeRepository {
 
   final Ref ref;
 
+  // TODO(yakitama5): Preferencesでまとめる
   static const _colorStyleKey = 'COLOR_STYLE';
   static const _uiStyleKey = 'UI_STYLE';
   static const _themeModeKey = 'THEME_MODE';
