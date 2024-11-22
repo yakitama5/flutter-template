e6ccaaf45ceae55ad629db39dc399de77f5f40dc
yakitama5 <rittitarou@yahoo.co.jp>
2024-11-22 21:22:02 +0900

    ♻️refactor: SharedPreferencesへの参照を共通化

---
+++ packages/cores/shared_preferences/lib/src/domain/value_object/preferences.dart
@@ -0,0 +1,10 @@
+enum Preferences<T> {
+  colorStyle('COLOR_STYLE', 'dynamicColor'),
+  uiStyle('UI_STYLE', 'system'),
+  themeMode('THEME_MODE', 'system');
+
+  const Preferences(this.key, this.defaultValue);
+
+  final String key;
+  final T defaultValue;
+}
