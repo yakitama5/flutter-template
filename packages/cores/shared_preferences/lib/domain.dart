0aa98d560055c4b250991c950c68f90ad1d224f6
yakitama5 <rittitarou@yahoo.co.jp>
2024-11-29 12:35:12 +0900

    ♻️refactor: SharedPreferencesへの参照を共通化

---
+++ packages/cores/shared_preferences/lib/domain.dart
@@ -0,0 +1,1 @@
+export 'src/domain/value_object/preferences.dart';
--- packages/cores/shared_preferences/lib/infrastructure.dart
+++ packages/cores/shared_preferences/lib/infrastructure.dart
@@ -1,1 +1,2 @@
+export 'src/infrastructure/state/preferences_notifier_provider.dart';
 export 'src/infrastructure/state/shared_preferences_provider.dart';
