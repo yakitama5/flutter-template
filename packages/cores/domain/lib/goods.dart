29a24b8eac1472f3d9b7f08dbdf7fe2da14eb717
yakitama5 <rittitarou@yahoo.co.jp>
2025-01-15 16:00:27 +0900

    ♻️refactor: ページングの仕組みを一部共通化

--- packages/cores/domain/lib/goods.dart
+++ packages/cores/domain/lib/goods.dart
@@ -4,5 +4,4 @@ export 'src/goods/interface/goods_repository.dart';
 export 'src/goods/state/goods_list_provider.dart';
 export 'src/goods/usecase/goods_usecase.dart';
 export 'src/goods/value_object/goods_fetch_query.dart';
-export 'src/goods/value_object/goods_fetch_response.dart';
 export 'src/goods/value_object/goods_sort_key.dart';
