ac402eac4f84388b634a2330d8f5a0167f7d8338
yakitama5 <rittitarou@yahoo.co.jp>
2025-01-22 16:47:13 +0900

    ♻️refactor: ログ整備

--- packages/cores/domain/lib/src/core/state/app_update_status_provider.dart
+++ packages/cores/domain/lib/src/core/state/app_update_status_provider.dart
@@ -1,6 +1,7 @@
 import 'package:cores_domain/src/core/interface/app_version_repository.dart';
 import 'package:cores_domain/src/core/state/app_build_config_provider.dart';
 import 'package:cores_domain/src/core/value_object/app_update_status.dart';
+import 'package:cores_domain/src/util/logger.dart';
 import 'package:riverpod/riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -18,9 +19,13 @@ Future<AppUpdateStatus> appUpdateStatus(Ref ref) async {
 
   // 比較してステータスを返却
   if (appVersion < forceUpdateAppVersion) {
+    logger.d('Update Required');
     return AppUpdateStatus.updateRequired;
   } else if (appVersion < latestAppVersion) {
+    logger.d('Update Possible');
     return AppUpdateStatus.updatePossible;
   }
+
+  logger.d('Using Latest');
   return AppUpdateStatus.usingLatest;
 }
