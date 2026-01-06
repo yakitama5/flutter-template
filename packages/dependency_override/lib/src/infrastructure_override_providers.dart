a26bcce73703b13dbf509916816c97bf138a8532
Yakuran <rittitarou@yahoo.co.jp>
2026-01-06 09:46:09 +0900

    🐛fix: パッケージのバージョンアップに伴う、dependency_overrideパッケージの修正
    原因:
    - パッケージのバージョンアップ
    
    対応:
    - 新バージョンに適用した記載方法に修正

--- packages/dependency_override/lib/src/infrastructure_override_providers.dart
+++ packages/dependency_override/lib/src/infrastructure_override_providers.dart
@@ -8,22 +8,25 @@ import 'package:infrastructure_mock/goods.dart';
 import 'package:infrastructure_shared_preferences/designsystem.dart';
 import 'package:infrastructure_shared_preferences/goods.dart';
 import 'package:infrastructure_shared_preferences/init.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:riverpod/misc.dart';
 
 Future<List<Override>> initializeInfrastructureProviders() async {
   return <Override>[
     // SharedPreferences
     ...await initializeSharedPreferencesProviders(),
     themeRepositoryProvider.overrideWith(SharedPreferencesThemeRepository.new),
-    goodsConfigRepositoryProvider
-        .overrideWith(SharedPreferencesGoodsConfigRepository.new),
+    goodsConfigRepositoryProvider.overrideWith(
+      SharedPreferencesGoodsConfigRepository.new,
+    ),
 
     // Firebase
     userRepositoryProvider.overrideWith(FirebaseUserRepository.new),
-    appMaintenanceRepositoryProvider
-        .overrideWith(RemoteConfigAppMaintenanceRepository.new),
-    appVersionRepositoryProvider
-        .overrideWith(RemoteConfigAppVersionRepository.new),
+    appMaintenanceRepositoryProvider.overrideWith(
+      RemoteConfigAppMaintenanceRepository.new,
+    ),
+    appVersionRepositoryProvider.overrideWith(
+      RemoteConfigAppVersionRepository.new,
+    ),
 
     // Mock
     goodsRepositoryProvider.overrideWith(MockGoodsRepository.new),
